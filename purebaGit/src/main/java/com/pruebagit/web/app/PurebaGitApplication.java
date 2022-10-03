f5a4a0a305758a9624671d18d73d3e6818210206
saulfl54 <saulfl54@educastur.es>
2022-10-03 21:00:04 +0200

    hola

--- purebaGit/src/main/java/com/pruebagit/web/app/PurebaGitApplication.java
+++ purebaGit/src/main/java/com/pruebagit/web/app/PurebaGitApplication.java
@@ -1,13 +1,22 @@
 package com.pruebagit.web.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class PurebaGitApplication {
+public class PurebaGitApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PurebaGitApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Hola");
+		
 	}
 
 }
