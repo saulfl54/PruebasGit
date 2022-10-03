e959ab1c077953c6191b1897ce66be91d2b4049d
saulfl54 <saulfl54@educastur.es>
2022-10-03 20:36:29 +0200

    Comenzamos

---
+++ purebaGit/src/main/java/com/pruebagit/web/app/PurebaGitApplication.java
@@ -0,0 +1,13 @@
+package com.pruebagit.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurebaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurebaGitApplication.class, args);
+	}
+
+}
---
+++ purebaGit/src/test/java/com/pruebagit/web/app/PurebaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebagit.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurebaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
